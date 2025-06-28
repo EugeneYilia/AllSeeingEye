2d7aabcbe2acfe8eda2efc655eb3c6069eb57c95
yichenliu <583076221@qq.com>
2025-06-29 01:26:35 +0800

    add k line part

--- src/main/kotlin/com/capitalEugene/model/kline/KlineBar.kt
+++ src/main/kotlin/com/capitalEugene/model/kline/KlineBar.kt
@@ -1,12 +1,25 @@
 package com.capitalEugene.model.kline
 
+import com.capitalEugene.common.utils.BigDecimalAsStringSerializer
+import kotlinx.serialization.Serializable
 import java.math.BigDecimal
 
+@Serializable
 data class KlineBar(
     val timestamp: Long,       // 毫秒时间戳
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val open: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val high: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val low: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val close: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val volume: BigDecimal
 )
