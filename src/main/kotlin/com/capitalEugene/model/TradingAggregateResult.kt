a2eae180970a3f77b835bf59e6aa49375481e1d6
yichenliu <583076221@qq.com>
2025-06-30 06:13:07 +0800

    trading result serializable

--- src/main/kotlin/com/capitalEugene/model/TradingAggregateResult.kt
+++ src/main/kotlin/com/capitalEugene/model/TradingAggregateResult.kt
@@ -1,14 +1,29 @@
 package com.capitalEugene.model
 
+import com.capitalEugene.common.utils.BigDecimalAsStringSerializer
+import kotlinx.serialization.Serializable
 import java.math.BigDecimal
 
+@Serializable
 data class TradingAggregateResult(
     val takeProfitCount: Int,
     val stopLossCount: Int,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val totalTakeProfitAmount: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val totalStopLossAmount: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val avgProfitPerTrade: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val avgLossPerTrade: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val capitalChangeRatio: BigDecimal,
+
+    @Serializable(with = BigDecimalAsStringSerializer::class)
     val avgHoldingDuration: BigDecimal,
 )
