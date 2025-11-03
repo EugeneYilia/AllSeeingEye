86fecbc47b331118ce138c6f52112285aee736b7
EugeneLiu <583076221@qq.com>
2025-11-03 16:23:14 +0800

    RENAME

--- src/main/kotlin/com/capitalEugene/backTest/sheng/ShengTradingFollowStrategyTester.kt
+++ src/main/kotlin/com/capitalEugene/backTest/sheng/ShengTradingFollowStrategyTester.kt
@@ -7,7 +7,6 @@ import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import kotlin.math.max
 
 /**
  * ShengMartinPyramidBacktest.kt
